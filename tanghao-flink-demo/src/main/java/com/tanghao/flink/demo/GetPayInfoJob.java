83c1cd015650139383a1c44dc937455f587d6cb8
canthny <canthnyq8@126.com>
2018-09-19 16:15:20 +0800

    flink simple demo commit

--- tanghao-flink-demo/src/main/java/com/tanghao/flink/demo/GetPayInfoJob.java
+++ tanghao-flink-demo/src/main/java/com/tanghao/flink/demo/GetPayInfoJob.java
@@ -1,8 +1,19 @@
 package com.tanghao.flink.demo;
 
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.formats.json.JsonNodeDeserializationSchema;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 
 import java.util.Properties;
@@ -18,12 +29,27 @@ public class GetPayInfoJob {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "192.168.0.102:9092");
-        properties.setProperty("group.id", "flink_consumer");
+        properties.setProperty("bootstrap.servers", "192.168.1.114:9092");
 
 
-        DataStream<String> stream = env
-                .addSource(new FlinkKafkaConsumer011<String>("flink-demo", new SimpleStringSchema(), properties));
+//        DataStream<String> stream = env.addSource(new FlinkKafkaConsumer011<String>("20180913", new SimpleStringSchema(), properties));
+        SingleOutputStreamOperator<ObjectNode> stream= env.addSource(new FlinkKafkaConsumer011("20180913", new JsonNodeDeserializationSchema(), properties));
+//        SingleOutputStreamOperator<Tuple2<String, Long>> stream = env.addSource(new FlinkKafkaConsumer011("20180913", new JsonNodeDeserializationSchema(), properties));
+        KeyedStream<Tuple2<String, Long>, Tuple> keyBy = stream.map(new MapFunction<ObjectNode, Tuple2<String, Long>>() {
+            public Tuple2<String, Long> map(ObjectNode jsonNodes) throws Exception {
+                System.out.println(jsonNodes.get("accountId").asText() + "==map====" + jsonNodes.get("amount").asLong());
+                return new Tuple2<String, Long>(jsonNodes.get("accountId").asText(), jsonNodes.get("amount").asLong());
+            }
+        }).keyBy(0);
+
+        keyBy.window(TumblingEventTimeWindows.of(Time.seconds(5))).reduce(new ReduceFunction<Tuple2<String, Long>>()
+        {
+            public Tuple2<String, Long> reduce(Tuple2<String, Long> v1, Tuple2<String, Long> v2) {
+                return new Tuple2<String, Long>(v1.f0, v1.f1 + v2.f1);
+            }
+        });
+
+//        stream.keyBy(0).timeWindow(Time.minutes(1)).sum(1);
         stream.print();
         env.execute();
     }
---
+++ tanghao-flink-demo/src/main/java/com/tanghao/flink/demo/TestAction.java
@@ -0,0 +1,42 @@
+package com.tanghao.flink.demo;
+
+import org.apache.flink.formats.json.JsonNodeDeserializationSchema;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
+
+/**
+ * @Author： Canthny
+ * @Description： 测试一些api
+ * @Date： Created in 2018/9/13 15:15
+ */
+public class TestAction {
+
+    public static void main(String[] args) {
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectNode initialKey = mapper.createObjectNode();
+            initialKey.put("index", 4);
+            byte[] serializedKey = mapper.writeValueAsBytes(initialKey);
+
+            ObjectNode initialValue = mapper.createObjectNode();
+            initialValue.put("word", "world");
+            byte[] serializedValue = mapper.writeValueAsBytes(initialValue);
+
+            JSONKeyValueDeserializationSchema schema = new JSONKeyValueDeserializationSchema(false);
+            ObjectNode deserializedValue = schema.deserialize(serializedKey, serializedValue, "", 0,
+                    0);
+            System.out.println(deserializedValue);
+
+            String testjson = "{\"accountId\":\"0716\",\"amount\":\"100\",\"eventTime\":\"2018/09/13 14:56:00\"}";
+            JsonNodeDeserializationSchema schema1 = new JsonNodeDeserializationSchema();
+            ObjectNode node = schema1.deserialize(testjson.getBytes());
+            System.out.println(node);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+
+
+    }
+}
