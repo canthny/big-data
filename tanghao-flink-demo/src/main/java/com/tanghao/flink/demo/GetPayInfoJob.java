c070c32c5e3d7de3fc3dc3b3693cf7229440c121
canthny <canthnyq8@126.com>
2018-09-30 10:46:39 +0800

    TumblingEventTimeWindows demo commit

--- tanghao-flink-demo/src/main/java/com/tanghao/flink/demo/GetPayInfoJob.java
+++ tanghao-flink-demo/src/main/java/com/tanghao/flink/demo/GetPayInfoJob.java
@@ -12,10 +12,17 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 /**
@@ -29,28 +36,49 @@ public class GetPayInfoJob {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "192.168.1.114:9092");
+        properties.setProperty("bootstrap.servers", "192.168.0.102:9092");
 
 
 //        DataStream<String> stream = env.addSource(new FlinkKafkaConsumer011<String>("20180913", new SimpleStringSchema(), properties));
-        SingleOutputStreamOperator<ObjectNode> stream= env.addSource(new FlinkKafkaConsumer011("20180913", new JsonNodeDeserializationSchema(), properties));
+        SingleOutputStreamOperator<ObjectNode> stream= env.addSource(new FlinkKafkaConsumer011("CountPerHourByAccountNo2", new JsonNodeDeserializationSchema(), properties));
 //        SingleOutputStreamOperator<Tuple2<String, Long>> stream = env.addSource(new FlinkKafkaConsumer011("20180913", new JsonNodeDeserializationSchema(), properties));
-        KeyedStream<Tuple2<String, Long>, Tuple> keyBy = stream.map(new MapFunction<ObjectNode, Tuple2<String, Long>>() {
+
+         SingleOutputStreamOperator<ObjectNode> objectNodeOperator = stream.assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<ObjectNode>(Time.seconds(15)) {
+            @Override
+            public long extractTimestamp(ObjectNode element) {
+                SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                Date eventTime = null;
+                try {
+                    eventTime = format.parse(element.get("eventTime").asText());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                return eventTime.getTime();
+            }
+        });
+
+        KeyedStream<Tuple2<String, Long>, Tuple> keyBy = objectNodeOperator.map(new MapFunction<ObjectNode, Tuple2<String, Long>>() {
             public Tuple2<String, Long> map(ObjectNode jsonNodes) throws Exception {
                 System.out.println(jsonNodes.get("accountId").asText() + "==map====" + jsonNodes.get("amount").asLong());
                 return new Tuple2<String, Long>(jsonNodes.get("accountId").asText(), jsonNodes.get("amount").asLong());
             }
         }).keyBy(0);
 
-        keyBy.window(TumblingEventTimeWindows.of(Time.seconds(5))).reduce(new ReduceFunction<Tuple2<String, Long>>()
-        {
-            public Tuple2<String, Long> reduce(Tuple2<String, Long> v1, Tuple2<String, Long> v2) {
-                return new Tuple2<String, Long>(v1.f0, v1.f1 + v2.f1);
-            }
-        });
+        testTumblingEventTimeWindows(keyBy);
 
 //        stream.keyBy(0).timeWindow(Time.minutes(1)).sum(1);
         stream.print();
         env.execute();
+    }
+
+    private static void testTumblingEventTimeWindows(KeyedStream<Tuple2<String, Long>, Tuple> keyBy) {
+        keyBy.window(TumblingEventTimeWindows.of(Time.seconds(5))).reduce(new ReduceFunction<Tuple2<String, Long>>()
+        {
+            public Tuple2<String, Long> reduce(Tuple2<String, Long> v1, Tuple2<String, Long> v2) {
+                Tuple2<String, Long> test = new Tuple2<String, Long>(v1.f0, v1.f1 + v2.f1);
+                System.out.println(test);
+                return test;
+            }
+        });
     }
 }
