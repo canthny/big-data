f32c0b7117fc87dfa3b5a9c460b82847cfdb8789
tanghao46 <tanghao46@jd.com>
2019-07-03 17:38:40 +0800

    springboot drools访问kies-server

--- drools_kie_demo/src/main/java/com/canthny/drools_kie_demo/test/DroolKieTestDemo.java
+++ drools_kie_demo/src/main/java/com/canthny/drools_kie_demo/test/DroolKieTestDemo.java
@@ -41,6 +41,7 @@ public class DroolKieTestDemo {
         List<Command<?>> commands = new LinkedList<>();
         commands.add(kieCommands.newInsert(userInfo, "userInfo"));
         commands.add(kieCommands.newFireAllRules());
+        //kie-server中发布的规则,以及设置的kiebase的id
         ServiceResponse<ExecutionResults> results = ruleServicesClient.executeCommandsWithResults("drools-kie-demo_1.0.0",
                 kieCommands.newBatchExecution(commands, "session1"));
         UserInfo value = (UserInfo) results.getResult().getValue("userInfo");
